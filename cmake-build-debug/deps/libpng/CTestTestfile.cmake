10a66976efda7c9a95c8280303b211eb8dabd912
Zeisen <negativezeisen@gmail.com>
2020-11-30 10:33:59 +0900

    patch for ougon cross version

--- cmake-build-debug/deps/libpng/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/FAMILY/CLionProjects/framedisplay/deps/libpng
-# Build directory: C:/Users/FAMILY/CLionProjects/framedisplay/cmake-build-debug/deps/libpng
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
